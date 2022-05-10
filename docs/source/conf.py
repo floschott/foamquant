015850c0a89e8f040e042ab10c8675db27578e30
floschott <82700428+floschott@users.noreply.github.com>
2022-05-10 15:49:23 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'foamquant'
+copyright = '2022, Florian Schott'
+author = 'Florian Schott'
 
 release = '0.1'
 version = '0.1.0'
