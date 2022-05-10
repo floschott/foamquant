45d8e35e8857aa7e3526b27f731aabd29748351b
floschott <82700428+floschott@users.noreply.github.com>
2022-05-10 11:03:40 +0200

    Update lumache.py

--- lumache.py
+++ lumache.py
@@ -1,23 +1,39 @@
 """
-Lumache - Python library for cooks and food lovers.
+Foamquant - Python library for processing time series of large 3D images of evolving foams.
 """
 
 __version__ = "0.1.0"
 
-
-class InvalidKindError(Exception):
-    """Raised if the kind is invalid."""
-    pass
-
-
-def get_random_ingredients(kind=None):
-    """
-    Return a list of random ingredients as strings.
-
-    :param kind: Optional "kind" of ingredients.
-    :type kind: list[str] or None
-    :raise lumache.InvalidKindError: If the kind is invalid.
-    :return: The ingredients list.
-    :rtype: list[str]
-    """
-    return ["shells", "gorgonzola", "parsley"]
+def RandomCmap(ncolors = 10000, namecmap = 'random_cmap', first_color_black=True):
+    
+    ''' 
+    Return a random colormap
+    Greatly inspired by: https://github.com/delestro/rand_cmap
+    
+    :param ncolors: The number of colors.
+    :type ncolors: int
+    :param namecmap: The random colormap name.
+    :type namecmap: str
+    :return: The random colormap random_cmap.
+    '''
+    
+    #Libraries
+    from matplotlib.colors import LinearSegmentedColormap
+    import colorsys
+    
+    #Random HSV list
+    randHSVcolors = [(np.random.uniform(low=0.0, high=1), 
+                      np.random.uniform(low=0.2, high=1), 
+                      np.random.uniform(low=0.9, high=1)) for i in range(ncolors)]
+    
+    #HSV to RGB
+    randRGBcolors = []
+    for HSVcolor in randHSVcolors:
+        randRGBcolors.append(colorsys.hsv_to_rgb(HSVcolor[0], HSVcolor[1], HSVcolor[2]))
+    #First color black
+    if first_color_black:
+        randRGBcolors[0] = [0, 0, 0]
+    #LinearSegmentedColormap
+    random_cmap = LinearSegmentedColormap.from_list(name, randRGBcolors, N=ncolors)
+    
+    return(random_cmap)
