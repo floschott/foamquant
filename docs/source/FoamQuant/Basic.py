41c44d50b5542a7db2f20e09c9b45cbd3b7d74dd
floschott <82700428+floschott@users.noreply.github.com>
2022-09-20 15:48:47 +0200

    Update Basic.py

--- docs/source/FoamQuant/Basic.py
+++ docs/source/FoamQuant/Basic.py
@@ -28,3 +28,13 @@ def ReadRaw(series, imi, rawdir, zN=800, top=0, bottom=None):
         
     # return 3D image
     return image
+
+
+
+
+def RangeList(i1, i2, verbose=False):
+    import numpy as np
+    List = np.uint8(np.linspace(i1,i2,i2-i1+1))
+    if verbose:
+        print(List)
+    return List
