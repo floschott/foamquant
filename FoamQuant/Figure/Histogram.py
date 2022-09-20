319df328da124ce3674f2fc6c9062781dd0acce9
floschott <82700428+floschott@users.noreply.github.com>
2022-09-20 11:22:44 +0200

    Update Histogram.py

--- FoamQuant/Figure/Histogram.py
+++ FoamQuant/Figure/Histogram.py
@@ -1,4 +1,14 @@
 def Histogram(image, histtitle=False):
+    """
+    Plot a histogram from a given grey-level image
+
+    Parameters
+    ----------
+        image : 3D numpy array
+
+        histtitle : str, tittle of the figure (default = False)
+    """
+    
     from skimage.exposure import histogram
     fig, ax = plt.subplots(ncols=1, figsize=(5, 5))
     hist, hist_centers = skimage.exposure.histogram(image)
