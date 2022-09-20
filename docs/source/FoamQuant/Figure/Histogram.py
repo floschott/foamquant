9aa838af3d72dc7e5f7c398e7ea16e8bbdb59a54
floschott <82700428+floschott@users.noreply.github.com>
2022-09-20 08:40:10 +0200

    Update Histogram.py

--- docs/source/FoamQuant/Figure/Histogram.py
+++ docs/source/FoamQuant/Figure/Histogram.py
@@ -1,4 +1,20 @@
 def Cut3D(image, histtitle=False):
+    ""
+    This function plot an grey-level histogram of the image.
+
+    Parameters
+    -----------
+        image : 3D numpy array
+            This is the grey-value image
+
+        histtitle : str (optional, default = None)
+            Add a tittle to the hitogram
+
+    Returns
+    --------
+        None
+    """
+   
     from skimage.exposure import histogram
     fig, ax = plt.subplots(ncols=1, figsize=(5, 5))
     hist, hist_centers = skimage.exposure.histogram(image)
