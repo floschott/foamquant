7f3eb81f2843742fe48d432030f8f2b583b283a9
floschott <82700428+floschott@users.noreply.github.com>
2022-09-22 12:46:47 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,15 +11,12 @@ version = '0.1.0'
 
 # -- General configuration
 
-!python3 -m pip install Sphinx
-
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'nbsphinx'
+    'sphinx.ext.intersphinx'
 ]
 
 intersphinx_mapping = {
